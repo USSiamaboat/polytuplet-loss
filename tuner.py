17bee8ced4bc036b0f04e0c1d62761797b1901b0
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2023-01-22 22:13:18 -0600

    Initial commit

--- tuner.py
+++ tuner.py
@@ -15,6 +15,7 @@ from transformers import (AlbertConfig, TFAlbertModel,
 import sentencepiece as spm
 
 from preprocess.preprocess import Preprocess
+from models.baseline import BaselineModel
 from models.polytuplet import PolytupletModel
 
 # Patch
@@ -29,19 +30,22 @@ model_name_map = {
 	"distilbert": 1,
 	"roberta": 2
 }
-MODEL_INDEX = model_name_map[sys.argv[1]]
-USE_MIXED = sys.argv[2] == "mixed"
 
-# for windows
+IS_BASELINE = sys.argv[1] == "baseline"
+MODEL_INDEX = model_name_map[sys.argv[2]]
+USE_MIXED = sys.argv[3] == "mixed"
+
+# Clear
 if os.name == 'nt':
 	os.system('cls')
 else:
 	os.system('clear')
 
 print("="*20)
-print("Running trainer with config")
-print(f"Model name {sys.argv[1]} and index {MODEL_INDEX}")
-print(f"Data mixing is {USE_MIXED}")
+print("Tuner Config")
+print("="*20)
+print(f"Model Information\n\tBaseline Arch.: {IS_BASELINE}\n\tModel Name: {sys.argv[2]}\n\tIndex: {MODEL_INDEX}\n")
+print(f"Data Information\n\tMixing: {USE_MIXED}")
 print("="*20)
 
 # Ensure path exists
@@ -59,20 +63,36 @@ preprocessor = Preprocess(df=df, model_index=MODEL_INDEX)
 
 # Generate dataset-ready tuples
 train_data, val_data = preprocessor.get_datasets(mixed=USE_MIXED)
+
+if IS_BASELINE:
+	train_data = ({"input_ids": train_data[0]["r_input_ids"], "attention_mask": train_data[0]["r_attention_mask"]}, train_data[1])
+	val_data = ({"input_ids": val_data[0]["r_input_ids"], "attention_mask": val_data[0]["r_attention_mask"]}, val_data[1])
+
 train_data = tf.data.Dataset.from_tensor_slices(train_data)
 val_data = tf.data.Dataset.from_tensor_slices(val_data)
 print("Preprocessing complete")
 
 print("Building model")
-model = PolytupletModel(preprocessor.CONTEXT_LEN, preprocessor.RESULT_LEN)
+if IS_BASELINE:
+	model = BaselineModel(preprocessor.RESULT_LEN)
+else:
+	model = PolytupletModel(preprocessor.CONTEXT_LEN, preprocessor.RESULT_LEN)
 
-model.tune_hyperparams(
-  train_data=train_data,
-  validation_data=val_data,
-  dropout_range=(0.0, 0.3),
-  learning_rate_range=(1e-7, 1e-5),
-  final_learning_rate_range=(1e-7, 1e-5),
-  alpha_range=(0.0, 10.0),
-  m_range=(0.0, 2.0),
-  hard_w_range=(0.0, 1.0)
-)
+if IS_BASELINE:
+	model.tune_hyperparams(
+		train_data=train_data,
+		validation_data=val_data,
+		dropout_range=(0.0, 0.5),
+		learning_rate_range=(1e-7, 1e-5)
+	)
+else:
+	model.tune_hyperparams(
+		train_data=train_data,
+		validation_data=val_data,
+		dropout_range=(0.0, 0.3),
+		learning_rate_range=(1e-7, 1e-5),
+		final_learning_rate_range=(1e-7, 1e-5),
+		alpha_range=(0.0, 10.0),
+		m_range=(0.0, 2.0),
+		hard_w_range=(0.0, 1.0)
+	)
